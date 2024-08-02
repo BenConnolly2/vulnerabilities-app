1cc18ec8631020ff0b5012df7ed66ce3d6c137ea
Ben Connolly <benconnollyofficial@gmail.com>
2024-08-02 09:25:27 +0100

    tidy up modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom';
 import VulnerabilityDetails from './VulnerabilityDetails';
-import { ModalProps } from '../types/modalProps';
+import { ModalProps } from '../types/modalProps.js';
 
 export default function Modal({ isOpen, onClose, vulnerability }: ModalProps) {
   if (!isOpen || !vulnerability) return null;
 
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-gray-600 bg-opacity-85 flex justify-center items-center rounded-lg">
-      <div className="bg-white p-8 rounded shadow-lg w-3/4 max-w-lg">
+      <div className="bg-white p-8 rounded shadow-lg w-3/4 max-w-3xl max-h-[90vh] overflow-y-auto">
         <VulnerabilityDetails vulnerability={vulnerability} />
-        <button onClick={onClose} className="bg-red-500 text-white p-2 rounded mt-4 hover:bg-red-600">
+        <button onClick={onClose} className="bg-red-500 text-white p-2 absolute top-4 right-4 rounded mt-4 hover:bg-red-600">
           Close
         </button>
       </div>
